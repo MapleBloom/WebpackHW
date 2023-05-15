179896ab9606ea7444332c0c8f0c41941057e05f
maxmar <maxmar@yandex.ru>
2023-05-16 02:15:57 +0300

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "plugins": [
+        'jsx-a11y',
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        indent: ["error", 2],
+        "no-console": "error"
+    }
+}
